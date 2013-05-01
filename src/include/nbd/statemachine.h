7d1f1c59b2b4250ae3892cc00a4164a81e752141
Wouter Verhelst <w@uter.be>
2013-05-01 11:59:49 +0200

    Some updates. Not ready yet, by a long shot.

---
+++ src/include/nbd/statemachine.h
@@ -0,0 +1,45 @@
+#ifndef NBD_STATEMACHINE_H
+#define NBD_STATEMACHINE_H
+
+typedef struct nbd_state_ nbd_state;
+typedef struct nbd_statemachine_ nbd_statemachine;
+typedef struct nbd_statemachine_instance_ nbd_statemachine_instance;
+
+typedef void(*enter_state_t)(nbd_state* state, nbd_statemachine_instance* inst);
+
+enum builtin_bits {
+	NBD_BIT_READ_READY = 1,
+	NBD_BIT_WRITE_READY,
+	NBD_BIT_COW_RESOLVE,
+	NBD_BIT_MULTIFILE_RESOLVE,
+	NBD_BIT_USER_START,
+}
+
+struct nbd_state_ {
+	struct nbd_state_private* priv;
+	nbd_statemachine* machine;
+	enter_state_t enter_state;
+}
+
+struct nbd_statemachine_state_ {
+	struct nbd_sm_instance_priv* priv;
+	nbd_statemachine* machine;
+	nbd_state* curstate;
+}
+
+nbd_statemachine* nbd_statemachine_new();
+nbd_statemachine_instance* nbd_statemachine_new_instance(nbd_statemachine*);
+
+int nbd_statemachine_add_bit(nbd_statemachine*);
+bool nbd_statemachine_get_bit(nbd_statemachine_instance*, int bit);
+bool nbd_statemachine_set_bit(nbd_statemachine_instance*, int bit);
+
+nbd_state* nbd_statemachine_add_state();
+bool nbd_statemachine_set_init_state(nbd_statemachine*, nbd_state*);
+
+nbd_transition* nbd_statemachine_add_transition(nbd_statemachine*, nbd_state* from, nbd_state* to);
+
+bool nbd_transition_require_bit(nbd_transition*, int bit);
+bool nbd_transition_forbid_bit(nbd_transition, int bit);
+
+#endif
