7e791148536a0ffb9180a656f664d57f7a1a757a
Wouter Verhelst <w@uter.be>
2013-05-01 14:14:02 +0200

    Make it compile

--- src/include/nbd/statemachine.h
+++ src/include/nbd/statemachine.h
@@ -1,9 +1,12 @@
 #ifndef NBD_STATEMACHINE_H
 #define NBD_STATEMACHINE_H
 
+#include <stdbool.h>
+
 typedef struct nbd_state_ nbd_state;
 typedef struct nbd_statemachine_ nbd_statemachine;
 typedef struct nbd_statemachine_instance_ nbd_statemachine_instance;
+typedef struct nbd_transition_ nbd_transition;
 
 typedef void(*enter_state_t)(nbd_state* state, nbd_statemachine_instance* inst);
 
@@ -13,20 +16,20 @@ enum builtin_bits {
 	NBD_BIT_COW_RESOLVE,
 	NBD_BIT_MULTIFILE_RESOLVE,
 	NBD_BIT_USER_START,
-}
+};
 
 struct nbd_state_ {
 	struct nbd_state_private* priv;
 	nbd_statemachine* machine;
 	enter_state_t enter_state;
-}
+};
 
 struct nbd_statemachine_instance_ {
 	struct nbd_sm_instance_priv* priv;
 	nbd_statemachine* machine;
 	nbd_state* curstate;
 	void* data;
-}
+};
 
 nbd_statemachine* nbd_statemachine_new();
 nbd_statemachine_instance* nbd_statemachine_new_instance(nbd_statemachine*);
--- src/statemachine.c
+++ src/statemachine.c
@@ -1,42 +1,41 @@
 #include <nbd/statemachine.h>
+#include <glib.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <assert.h>
 
 struct nbd_statemachine_ {
 	int max_bit;
 	int instances;
 	nbd_state* init_state;
-}
+};
 
 struct nbd_sm_instance_priv {
 	GArray* bits;
-}
+};
 
-struct nbd_state_transition {
+struct nbd_transition_ {
 	nbd_state* to;
 	GArray* bits_require;
 	GArray* bits_forbid;
-}
+};
 
 struct nbd_state_private {
 	GArray* out;
-}
+};
 
 nbd_statemachine* nbd_statemachine_new() {
-	nbd_statemachine* retval = calloc(sizeof(nbd_statemachine*, 1));
+	nbd_statemachine* retval = calloc(sizeof(nbd_statemachine*), 1);
 	retval->max_bit = NBD_BIT_USER_START - 1;
 }
 
-nbd_statemachine_state* nbd_statemachine_new_instance(nbd_statemachine* mach) {
-	nbd_statemachine_state* retval = calloc(sizeof(nbd_statemachine_state), 1);
+nbd_statemachine_instance* nbd_statemachine_new_instance(nbd_statemachine* mach) {
+	nbd_statemachine_instance* retval = calloc(sizeof(nbd_statemachine_instance), 1);
 	mach->instances++;
-	retval->priv = calloc(sizeof(struct nbd_sm_instance_priv));
-	retval->priv->bits = g_array_new(FALSE, TRUE, sizeof(uint64_t))
+	retval->priv = calloc(sizeof(struct nbd_sm_instance_priv), 1);
+	retval->priv->bits = g_array_new(FALSE, TRUE, sizeof(uint64_t));
 	g_array_set_size(retval->priv->bits, (mach->max_bit / 64) + 1);
 	retval->curstate = mach->init_state;
-}
-
-void nbd_statemachine_set_bit_immediate(nbd_statemachine_instance* inst, int bit) {
-	nbd_statemachine_set_bit(inst, bit);
-	nbd_statemachine_check_bits(inst);
 }
 
 void nbd_statemacine_set_bit(nbd_statemachine_instance* inst, int bit) {
@@ -44,19 +43,24 @@ void nbd_statemacine_set_bit(nbd_statemachine_instance* inst, int bit) {
 
 	int bit_apos = bit / 64;
 	int bit_bpos = bit % 64;
-	g_array_index(inst->bits, uint64_t, bit_apos) |= 1 << bit_bpos;
+	g_array_index(inst->priv->bits, uint64_t, bit_apos) |= 1 << bit_bpos;
 }
 
 void nbd_statemachine_check_bits(nbd_statemachine_instance* inst) {
 	for(int i=0; i<inst->curstate->priv->out->len; i++) {
-		struct nbd_state_transition* t = g_array_index(inst->curstate->priv->out, nbd_state_transition*, i)
+		nbd_transition* t = g_array_index(inst->curstate->priv->out, nbd_transition*, i);
 		for(int j=0; j<=inst->machine->max_bit / 64; j++) {
-			if( ((g_array_index(inst->bits, uint64_t, j) & g_array_index(t->bits_require, uint64_t, j)) == g_array_index(t->bits_require, uint64_t, j))
-			 && ((g_array_index(inst->bits, uint64_t, j) & g_array_index(t->bits_forbid,  uint64_t, j)) == 0)) {
-				inst->cur_state = t->to;
-				inst->cur_state->enter_state(inst->curstate, inst);
+			if( ((g_array_index(inst->priv->bits, uint64_t, j) & g_array_index(t->bits_require, uint64_t, j)) == g_array_index(t->bits_require, uint64_t, j))
+			 && ((g_array_index(inst->priv->bits, uint64_t, j) & g_array_index(t->bits_forbid,  uint64_t, j)) == 0)) {
+				inst->curstate = t->to;
+				inst->curstate->enter_state(inst->curstate, inst);
 				return;
 			}
 		}
 	}
+}
+
+void nbd_statemachine_set_bit_immediate(nbd_statemachine_instance* inst, int bit) {
+	nbd_statemachine_set_bit(inst, bit);
+	nbd_statemachine_check_bits(inst);
 }
